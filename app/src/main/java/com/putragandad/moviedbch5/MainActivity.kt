e61c5b2a5a82d0be37e0203f68440be12d769aa7
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-05-17 15:15:33 +0700

    Configure NavGraph and set Bottom Nav Bar + Dark Theme

--- app/src/main/java/com/putragandad/moviedbch5/MainActivity.kt
+++ app/src/main/java/com/putragandad/moviedbch5/MainActivity.kt
@@ -2,10 +2,20 @@ package com.putragandad.moviedbch5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.putragandad.moviedbch5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavController = this.findNavController(R.id.NavHostFragment)
+
+        binding.bottomNavigation.setupWithNavController(bottomNavController)
     }
 }

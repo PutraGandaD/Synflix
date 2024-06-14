e7b12fcd32c543403c19cce6ecc55ec8f6be3f64
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-06-15 01:05:07 +0700

    Clean all preferences for each new account register

--- app/src/main/java/com/putragandad/moviedbch5/domain/usecases/users/UserRegisterUseCase.kt
+++ app/src/main/java/com/putragandad/moviedbch5/domain/usecases/users/UserRegisterUseCase.kt
@@ -4,6 +4,7 @@ import com.putragandad.moviedbch5.domain.repositories.users.UserAuthRepository
 
 class UserRegisterUseCase(private val userAuthRepository: UserAuthRepository) {
     operator suspend fun invoke(fullname: String, email: String, password: String) {
+        userAuthRepository.deleteAllPreferences()
         userAuthRepository.registerUser(fullname, email, password)
     }
 }
